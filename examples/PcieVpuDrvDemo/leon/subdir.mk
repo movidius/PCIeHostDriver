a4d4d94dd373112f071efc39f703d7e350bc6577
aboji <andrei-george.boji@intel.com>
2023-01-27 14:47:33 +0200

    Removed PcieVpuDrvDemo as it is obsolete and incompatible with latest MDK releases

--- examples/PcieVpuDrvDemo/leon/subdir.mk
+++
@@ -1,3 +0,0 @@
-my-app-srcs := main.c rtems_config.c
-
-srcs-los-y += $(my-app-srcs)
--- examples/PcieVpuDrvDemo/newMakefile.mk
+++
@@ -1,7 +0,0 @@
-MDK_INSTALL_DIR ?= ../../../../../common
-
-include $(MDK_INSTALL_DIR)/../system/build/mdk.mk
-
-TEST_HW_PLATFORM   := "MV0262_MA2480"
-TEST_TYPE          := "MANUAL"
-TEST_TAGS := "MDK_DRIVERS, MA2480, WIN_MX"
